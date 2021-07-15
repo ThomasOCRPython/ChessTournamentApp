0c271d9b5f64f6c86bce4145a2fff4cc26033b6e
Thomas2020 <thomas.duval2020@campus-eni.fr>
2021-07-15 14:12:43 +0200

    mise en place dossier MVC

---
+++ models/tournament.py
@@ -0,0 +1,15 @@
+class Tournament:
+    def __init__(self):
+        self.name=""
+        self.place=""
+        self.date=""
+        self.nb_of_urns=""
+        self.tours=""
+        self.players=""
+        self.time_control=""
+        self.description=""
+
+
+
+
+    
